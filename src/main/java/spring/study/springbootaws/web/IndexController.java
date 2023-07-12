cbe7fd3993d00080ac71b3c8c82f1a1d7f25380f
ahma0 <dsd932@naver.com>
2023-07-12 17:53:43 +0900

    어노테이션 기반으로 개선하기

--- src/main/java/spring/study/springbootaws/web/IndexController.java
+++ src/main/java/spring/study/springbootaws/web/IndexController.java
@@ -5,25 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import spring.study.springbootaws.config.auth.LoginUser;
 import spring.study.springbootaws.config.auth.dto.SessionUser;
 import spring.study.springbootaws.service.posts.PostsService;
 import spring.study.springbootaws.web.dto.PostsResponseDTO;
-
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {      // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있습니다.
+    public String index(Model model, @LoginUser SessionUser user) {      // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있습니다.
                                             // 여기서는 postsService.findAllDesc( )로 가져온 결과를
                                             // posts로 index.mustache에 전달합니다
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null)
             model.addAttribute("userName", user.getName());
